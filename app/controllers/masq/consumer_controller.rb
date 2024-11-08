b74937e7c54119cce3bdac184dc579ab794d9c90
Peter Boling <peter.boling@gmail.com>
2024-11-08 17:54:50 +0700

    ⬆️ Rails 5.0 => 5.1 upgrade: before_filter => before_action

--- app/controllers/masq/consumer_controller.rb
+++ app/controllers/masq/consumer_controller.rb
@@ -1,7 +1,7 @@
 module Masq
   class ConsumerController < BaseController
 
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
 
     def start
       begin
