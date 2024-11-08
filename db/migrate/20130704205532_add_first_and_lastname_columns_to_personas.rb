e49d4e77c1d8cd33493c6c2c41aab36b860bb038
Peter Boling <peter.boling@gmail.com>
2024-11-08 16:48:58 +0700

    ⬆️ Rails 4 => 5 upgrade: Migrations

--- db/migrate/20130704205532_add_first_and_lastname_columns_to_personas.rb
+++ db/migrate/20130704205532_add_first_and_lastname_columns_to_personas.rb
@@ -1,4 +1,4 @@
-class AddFirstAndLastnameColumnsToPersonas < ActiveRecord::Migration
+class AddFirstAndLastnameColumnsToPersonas < ActiveRecord::Migration[4.2]
   def change
     if table_exists?(:masq_personas) && !column_exists?(:masq_personas, :firstname)
       add_column :masq_personas, :firstname, :string
--- db/migrate/20130807060329_change_open_id_associations_server_url_column_type.rb
+++ db/migrate/20130807060329_change_open_id_associations_server_url_column_type.rb
@@ -1,4 +1,4 @@
-class ChangeOpenIdAssociationsServerUrlColumnType < ActiveRecord::Migration
+class ChangeOpenIdAssociationsServerUrlColumnType < ActiveRecord::Migration[4.2]
   def up
     # Only run if the column type is not already a varchar/string.
     if Masq::Association.columns_hash['server_url'].type != :string
@@ -7,7 +7,7 @@ class ChangeOpenIdAssociationsServerUrlColumnType < ActiveRecord::Migration
       rename_column(:masq_open_id_associations, :server_url, :binary_server_url)
       add_column(:masq_open_id_associations, :server_url, :string, :limit => 2000)
 
-      # COpy the data over since we cant change a binary column type
+      # Copy the data over since we cant change a binary column type
       Masq::Association.all.each do |record|
         record.update_attribute(:server_url, record.binary_server_url.to_s)
       end
