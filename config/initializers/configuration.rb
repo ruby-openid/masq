45815642a74f8189871296a408a522ba809a8f59
Peter Boling <peter.boling@gmail.com>
2024-11-08 17:08:21 +0700

    ✨ Allow ERB within YAML for config file

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -1,3 +1,5 @@
 Masq::Engine.configure do
-  config.masq = YAML.load(File.read("#{Rails.root}/config/masq.yml"))[Rails.env]
+  config_file_path = Rails.root.join("config", "masq.yml")
+  parsed_erb = ERB.new(config_file_path.read).result
+  config.masq = YAML.load(parsed_erb)[Rails.env]
 end
